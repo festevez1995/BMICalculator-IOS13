0137a7a918fd819124d532351ee211fd381debce
festevez1995 <estevezfernando1995@gmail.com>
2024-10-28 22:14:29 -0700

    Finished up connecting UI comments with code.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,19 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func HeightSliderTriggered(_ sender: UISlider) {
-        print(sender.value)
+        heightLabel.text = String(format: "%0.2f", sender.value) + "m"
+        
+       // print(String(format: "%0.2f", sender.value))
     }
     
     @IBAction func WeightSliderTriggered(_ sender: UISlider) {
-        print(sender.value)
+        weightLabel.text = String(Int(sender.value)) + "Kg"
+       // print(Int(sender.value))
 
     }
     
+    @IBAction func calculationPressed(_ sender: Any) {
+        let inputHeight = heightSlider.value
+        let inputWeight = weightSlider.value
+        
+        let bmi = calculateBMI(height: inputHeight, weight: inputWeight)
+        
+        print(bmi)
+    }
+    
+    func calculateBMI(height: Float, weight: Float) -> Float {
+        let bmi = weight / (height * height)
+        return bmi
+    }
 }
 
